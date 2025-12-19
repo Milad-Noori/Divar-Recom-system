2148f8eaa2c9bd38d4e9fc106345ef58aed17ae7
milad <seyeadmiladnouri@gmail.com>
2025-12-20 00:21:16 +0330

    df1

--- divar-recom-system.py
+++ divar-recom-system.py
@@ -1,17 +1,18 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.Linear_Regression import LinearRegression
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
 # df.shape()
 # df.head()
 # df.describe()
 # df.innull().sum()
-data = pd.read_csv("")
-
-y=
-X_test,Y_test , X_train,Y_train
-lr=LinearRegression()
-lr.fit(Y_tesst,Y_train)
-y-pred =lr.predict(X_test)
+data = pd.read_csv("HouseNew.csv")
+df1 = pd.DataFrame(data)
+df1.isnull().sum()
+print(df1)
+# X_test,Y_test , X_train,Y_train = train_test_split(X,Y ,test_size = 0.2, random_state = 0)
+# lr=LinearRegression()
+# lr.fit(Y_tesst,Y_train)
+# y_pred =lr.predict(X_test)
