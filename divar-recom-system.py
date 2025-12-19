8a611c47d6f0ff158e31dbdfd0845915aeffb859
milad <seyeadmiladnouri@gmail.com>
2025-12-19 23:54:03 +0330

    lr

--- divar-recom-system.py
+++ divar-recom-system.py
@@ -4,8 +4,14 @@ import matplotlib.pyplot as plt
 from sklearn.Linear_Regression import LinearRegression
 from sklearn.model_selection import train_test_split
 
-df.shape()
-df.head()
-df.describe()
-df.innull().sum()
+# df.shape()
+# df.head()
+# df.describe()
+# df.innull().sum()
 data = pd.read_csv("")
+
+y=
+X_test,Y_test , X_train,Y_train
+lr=LinearRegression()
+lr.fit(Y_tesst,Y_train)
+y-pred =lr.predict(X_test)
