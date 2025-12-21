1c249a89a50bfd96544c11641e8d1e9043b13d5b
milad <seyeadmiladnouri@gmail.com>
2025-12-21 21:55:06 +0330

    parking

--- divar-recom-system.py
+++ divar-recom-system.py
@@ -1,5 +1,3 @@
-from email.headerregistry import Address
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -10,15 +8,26 @@ from sklearn.model_selection import train_test_split
 from tkinter import *
 window = Tk()
 window.title("House-price")
-window.geometry("1300x400")
+window.geometry("1300x500")
 label = Label(window, text="House-price",background="gray",foreground="black",font=("Tahoma",20)).pack()
 name_model = Label(window,text="Model name :",padx=10,pady=10)
 name_model.place(x=50,y=100)
 Address = Label(window,text="Address :",padx=10,pady=10)
 Address.place(x=50,y=150)
-Button = Button(window,text = "Predict",padx=15,pady=15)
-Button.place(x=600,y=300)
+Area = Label(window,text = "Area :",padx=15,pady=15)
+Area.place(x=50,y=200)
+Floor = Label(window,text = "Floor :",padx=15,pady=15)
+Floor.place(x=50,y=250)
+Parking = Label(window,text = "Parking :",padx=15,pady=15)
+Parking.place(x=50,y=300)
 
+
+
+
+
+
+Button = Button(window,text = "Predict",padx=15,pady=15)
+Button.place(x=600,y=400)
 window.mainloop()
 
 
